bb08304b99045645636d402a53d43ce442952497
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2023-11-01 19:35:48 +0200

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+galleryListEl.addEventListener('click', onImgClick);
+
+
+function galleryItemTemplate({preview, original, description}) {
+    return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </li>`
+}
+
+function galleryItemsTemplate(galleryArr) {
+    return galleryArr.map(galleryItemTemplate).join('');
+}
+
+function renderGallery(galleryArr) {
+    const markup = galleryItemsTemplate(galleryArr);
+    galleryListEl.insertAdjacentHTML('afterbegin', markup);
+}
+
+renderGallery(galleryItems);
+
+
+function onImgClick(event) {
+    event.preventDefault()
+    const url = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${url}" width="800" height="600">
+    `, {
+    onShow: (instance) => {
+        document.addEventListener('keydown', function(event) {
+            if (event.key === 'Escape') {
+                instance.close();
+            }
+        })
+    }
+})
+    instance.show()
+}
+
+
+
