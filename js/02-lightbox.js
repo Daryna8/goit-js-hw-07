8801cb0054661bb2161fe759fe8242f775d8e0ae
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2023-11-02 17:28:12 +0200

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+
+function galleryItemTemplate({preview, original, description}) {
+    return `<li class="gallery__item">
+             <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+             </a>
+            </li>`
+}
+
+function galleryItemsTemplate(galleryArr) {
+    return galleryArr.map(galleryItemTemplate).join('');
+}
+
+function renderGallery(galleryArr) {
+    const markup = galleryItemsTemplate(galleryArr);
+    galleryListEl.insertAdjacentHTML('afterbegin', markup);
+}
+
+renderGallery(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', capttionDelay: 250, });
